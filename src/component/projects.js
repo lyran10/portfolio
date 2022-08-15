382df316d82b14a333256ce51f7a3bff1d888d58
lyran10 <liranramekar629@gmail.com>
2022-08-15 12:56:39 +0300

    added

--- src/component/projects.js
+++ src/component/projects.js
@@ -18,7 +18,7 @@ export const Projects = () => {
                 <span className="text card-text wordSpace">This app is used to track day to day tasks. Also can put a description for the task. A Database
                 is used to store the data. Also can delete or edit the task.</span>
                 <div style={{marginTop:"30px"}}>
-                <a href="https://create-weather-application.herokuapp.com" target="_blank"><button style={{width:"100%",border:"solid 2px #D2042D"}} className="btn button text-dark">Trial</button></a>
+                <a href="https://app-two-do-list.herokuapp.com" target="_blank"><button style={{width:"100%",border:"solid 2px #D2042D"}} className="btn button text-dark">Trial</button></a>
                 </div>
               </div>
             </div>
@@ -28,7 +28,7 @@ export const Projects = () => {
                 <h5 className="texthead card-title">Weather Application</h5>
                 <span className="text card-text wordSpace">This app gives information to the user about the current weather and five days forecast of any city around the world in Fahrenheit and Celsius temperature.</span>
                 <div style={{width:"100%"}} className="weatherBtn">
-                <a href="https://app-two-do-list.herokuapp.com" target="_blank"><button style={{width:"100%",border:"solid 2px #D2042D"}} className="btn button text-dark">Trial</button></a>
+                <a href="https://create-weather-application.herokuapp.com" target="_blank"><button style={{width:"100%",border:"solid 2px #D2042D"}} className="btn button text-dark">Trial</button></a>
                 </div>
               </div>
             </div>
@@ -40,3 +40,4 @@ export const Projects = () => {
       </section>
   )
 }
+
